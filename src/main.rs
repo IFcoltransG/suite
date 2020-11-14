33195ec251514f96a6f1537a8f3a0fe4d5891af8
IFcoltransG <47414286+IFcoltransG@users.noreply.github.com>
2020-11-14 23:37:01 +1300

    Adjust capitalisation for effect

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,3 @@
 fn main() {
-    println!("Smite, the Simple Modern Intuitive Text Editor, is not done yet, because I wrote this message in Nano. Stay tuned!");
+    println!("smite, the Simple Modern Intuitive Text Editor, is not done yet, because I wrote this message in nano. Stay tuned!");
 }
