d4925212d6d5c835b7f2271090a62068c3ed11bf
IFcoltransG <47414286+IFcoltransG@users.noreply.github.com>
2020-11-15 21:11:26 +1300

    Rename from smite to suite

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,3 @@
 fn main() {
-    println!("smite, the Simple Modern Intuitive Text Editor, is not done yet, because I wrote this message in nano. Stay tuned!");
+    println!("suite, the Simple Uncompromisingly Intuitive Text Editor, is not done yet, because I wrote this message in nano. Stay tuned!");
 }
